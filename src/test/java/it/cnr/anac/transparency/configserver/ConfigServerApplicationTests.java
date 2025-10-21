fcf845c99f0f4c203780bd7bd53b1591360fe8a8
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2025-10-21 12:24:36 +0200

    Migrazione dei test da h2 a postgres con test-container.

--- src/test/java/it/cnr/anac/transparency/configserver/ConfigServerApplicationTests.java
+++ src/test/java/it/cnr/anac/transparency/configserver/ConfigServerApplicationTests.java
@@ -18,9 +18,18 @@ package it.cnr.anac.transparency.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.PostgreSQLContainer;
 
+@Testcontainers
 @SpringBootTest
 class ConfigServerApplicationTests {
+
+  @Container
+  @ServiceConnection
+  static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
   @Test
   void contextLoads() {
